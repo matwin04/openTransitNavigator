e697fc8c78b0d91bcca85d59dfb9cd704f22839c
frostie <mattwiner04@gmail.com>
2025-05-29 11:59:01 -0700

    Update db.js

--- db.js
+++ db.js
@@ -16,15 +16,26 @@ async function setupDB() {
                 timezone TEXT,
                 lang TEXT,
                 phone TEXT
-            );
+            )`;
+        await sql`
             CREATE TABLE stops (
                 stop_id TEXT PRIMARY KEY,
                 name TEXT NOT NULL,
                 code TEXT,
                 description TEXT,
-                geom GEOMETRY(POINT,4326) NOT NULL,
-            );
-            `
+                geom GEOMETRY(POINT,4326) NOT NULL
+            )`;
+        await sql`
+            CREATE TABLE routes (
+                route_id    TEXT PRIMARY KEY,
+                agency_id   TEXT NOT NULL,
+                short_name  TEXT NOT NULL,
+                long_name   TEXT NOT NULL,
+                description TEXT,
+                type        INTEGER,
+                color       TEXT,
+                text_color  TEXT
+            )`;
 
     } catch (error) {
         console.log(error);
